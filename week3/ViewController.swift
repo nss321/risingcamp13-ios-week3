15253fd558854b6a5976b7fea43c28ba528407cf
BAE <lexon1209@gmail.com>
2023-01-04 00:56:32 +0900

    Initial Commit

---
+++ week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week3
+//
+//  Created by BAE on 2023/01/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
